f86a8fb2300e9dc0e0187f603c737d2c21603a08
MaxiFrank7 <maximilianofrank007@gmail.com>
2026-08-31 19:05:32 -0300

    Actualizacion carpeta AA1

--- TUIA_Segundo/2Cuatrimestre/4C_MachineLearning/Unidad 6_ MLOps2/ejemplo-streamlit-iris/ejecutar_streamlit.bat
+++
@@ -1,3 +0,0 @@
-cd c:\program files\python311
-python -m streamlit run C:\AA1\MLOps\app_iris.py
-pause
--- TUIA_Segundo/2Cuatrimestre/4C_MachineLearning/Unidad 6_ MLOps2/wine-app/ejecutar_streamlit.bat
+++
@@ -1,3 +0,0 @@
-cd c:\program files\python311
-python -m streamlit run C:\AA1\MLOps\wine-app\app.py
-pause
